d0532d09f7bb901df0141a5a2ccdd1c5429b4f12
Sjimi <roose.dimitri@gmail.com>
2013-06-18 12:14:06 +0200

    added Twig and Monolog

--- web/index.php
+++ web/index.php
@@ -4,5 +4,16 @@ require_once(__DIR__.'/../vendor/autoload.php');
 
 $app = new Silex\Application();
 
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+	'twig.path' => __DIR__.'/views',
+	'twig.options' => array(
+		'debug' => true
+	)
+));
+$app['twig']->addExtension(new Twig_Extension_Debug());
+
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+	'monolog.logfile' => __DIR__.'/../log/development.log'
+));
 
 $app->run();
