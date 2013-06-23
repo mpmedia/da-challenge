45715ad98282d4326f17859b20208c13206e1271
Sjimi <roose.dimitri@gmail.com>
2013-06-23 22:22:24 +0200

    added mail client-ish look; added functionality to fetch a single mail

--- src/app.php
+++ src/app.php
@@ -16,63 +16,27 @@ $app->match('/', function (Symfony\Component\HttpFoundation\Request $request) us
         $form->bind($request);
 
         if ($form->isValid()) {
-            // setup the Mailr and share it for the rest of the application
 
-            /*$app['mailr'] = $app->share( function() use($form) {
-                return new DarwinAnalytics\Mailr('{imap.gmail.com:993/imap/ssl}','INBOX',$form->getData()['email'],$form->getData()['password']);
-            });*/
+            // use the service provider for imap injection
+            $app->register(new DarwinAnalytics\Provider\ImapServiceProvider());
 
-            $app->register(new DarwinAnalytics\Provider\ImapServiceProvider(), array(
-                'imap.provider' => '{imap.gmail.com:993/imap/ssl}'
-            ));
+            $app['imap']->open('{imap.gmail.com:993/imap/ssl}', $form->getData()['email'], $form->getData()['password']);
 
-            // if all went okay redirect to /messages
-            $request = Symfony\Component\HttpFoundation\Request::create('/messages', 'GET');
-            return print_r($app['imap']);
-            /*
-            $app['monolog']->addDebug('Fetching mails for '.$form->getData()['email'].'.');
-            
-            if($inbox = imap_open('{imap.gmail.com:993/imap/ssl}INBOX',$form->getData()['email'],$form->getData()['password'])) {
+            $request = Symfony\Component\HttpFoundation\Request::create('/mails', 'GET');
 
-                $all = imap_search($inbox, 'ALL');
-                $messages = array();
-
-                if($all) {
-                    foreach($all as $messageNumber) {
-                        $overview = imap_fetch_overview($all, $messageNumber, 0);
-                        $messages[$messageNumber]->body = imap_fetchstructure($inbox, $messageNumber);
-                        $messages[$messageNumber]->subject = $overview[0]->subject;
-                    }
-                }
-
-                $request = Symfony\Component\HttpFoundation\Request::create('/messages', 'GET', array(
-                    'messages' => $messages
-                    ));
-
-                return $app->handle($request); 
-            } else {
-                throw new Exception(imap_last_error().'.', 1);
-            }*/
+            return $app->handle($request);
         }
     }
     return $app['twig']->render('index.html.twig', array('form' => $form->createView()));
 });
 
-$app->get('/messages', function(Symfony\Component\HttpFoundation\Request $request) use ($app) {
-    return $app['twig']->render('messages.html.twig', array(
-        //'messages' => $request->get('messages')
+$app->get('/mails', function(Symfony\Component\HttpFoundation\Request $request) use ($app) {
+    return $app['twig']->render('mails.html.twig', array(
+        'mails' => $app['imap']->getMailHeaders()
         ));
 });
 
 $app->error(function (\Exception $e, $code) use ($app) {
-    $app['monolog']->addDebug('Error with code '.$code);
-    switch ($code) {
-        case 404:
-        $message = 'The requested page could not be found.';
-        break;
-        default:
-        $message = 'We are sorry, but something went terribly wrong.';
-    }
     return new Symfony\Component\HttpFoundation\Response($app['twig']->render('404.html.twig', array( 'content' => $e->getMessage())), $code);
 });
 
